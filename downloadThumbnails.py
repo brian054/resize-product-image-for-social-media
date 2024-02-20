28978b3a653e20fa6e5c6af2ecce83f65f24ee26
brian054 <brian.rowell54@gmail.com>
2024-02-19 22:24:18 -0500

    Daytona 500 = not much code to commit

--- downloadThumbnails.py
+++ downloadThumbnails.py
@@ -2,12 +2,20 @@ import os
 import json
 import requests
 
+# Download Gallery Img's - just make a new function 
+# Just put them in Vendor_OG_Images/Vendor/productID/GalleryImgs/
+#
+# Then generateCSVData for those GalleryImg's
+#   = where they'll be saved??? 
+#   = then resize them using the same algorithm that the retailsite images are 
+#   = using. At that point, we're good just need to generate maybe 10 
+#   = vendors in advance, then look into importing and talk about in stand up
+
 json_file_path = 'JSON/AuburnLeather.json'
 
 # Extract name for folder creation
 base_name = os.path.basename(json_file_path)
 json_file_name = os.path.splitext(base_name)[0]
-
 
 def download_image(image_url, save_path):
     response = requests.get(image_url)
@@ -39,4 +47,24 @@ def process_products(json_file_path):
         print(f"Downloaded {image_file_name} to {dir_path}")
     print(count)
 
+def download_gallery_images(json_data):
+    with open(json_data, 'r') as file:
+        items = json.load(file)
+
+    for item in items:
+        folder_name = str(item['id'])
+        os.makedirs(f'Vendor_OG_Images/{json_file_name}/{folder_name}/GalleryImages/', exist_ok=True)
+        for image in item['images']:
+            image_url = image['url']
+            image_id = str(image['id']) # for the name
+            file_name = os.path.join(f'Vendor_OG_Images/{json_file_name}/{folder_name}/GalleryImages/', image_id + '.png')
+            response = requests.get(image_url)
+            if response.status_code == 200:
+                with open(file_name, 'wb') as f:
+                    f.write(response.content)
+            else:
+                print("Failed to download image {image_id} from {image_url}")
+
 process_products(json_file_path)
+json_gallery = 'JSON/AuburnLeather_Gallery.json'
+download_gallery_images(json_gallery)
